828f3986da97454674c986c5e4dbf459cdcdaef4
Giovanni Marangi <giovanni.marangi22@gmail.com>
2025-10-16 16:26:52 +0000

    tested various decoders

--- py/custom_tasks.py
+++ py/custom_tasks.py
@@ -1,8 +1,43 @@
 from terratorch.tasks import SemanticSegmentationTask
+import torch
 
 class DiffLRSemanticSegmentationTask(SemanticSegmentationTask):
+
+    # Overriding this method of TerratorchTask, parent class of SemanticSegmentationTask.
+    # Pytorch Lightning uses it to build the optimizer. Sould return a dict specifying:
+    # - the optimizer
+    # - the lr scheduler (optional)
+    # - eventually, their hparams
     def configure_optimizers(self):
-        params = [
-            {"params": self.model.backbone.parameters(), "lr": 1e-6, "weight_decay": 1e-3},
-        ]
-        pass
+        opt_hparams = self.hparams.get("optimizer_hparams", {})
+        scheduler = self.hparams.get("scheduler", None)
+        scheduler_hparams = self.hparams.get("scheduler_hparams", {})
+        
+        if self.model.decoder._get_name() == "UperNetDecoder":
+            params = [
+                {"params": self.model.encoder.parameters(), "lr": opt_hparams.get("encoder_lr", 1e-6)},
+                {"params": self.model.decoder.parameters(), "lr": opt_hparams.get("decoder_lr", 1e-4)},
+                {"params": self.model.head.parameters(), "lr": opt_hparams.get("head_lr", 1e-4)}
+            ]
+        else:
+            params = [
+                {"params": self.model.encoder.parameters(), "lr": opt_hparams.get("encoder_lr", 1e-6)},
+                {"params": self.model.neck.parameters(), "lr": opt_hparams.get("decoder_lr", 1e-4)},
+                {"params": self.model.decoder.parameters(), "lr": opt_hparams.get("decoder_lr", 1e-4)},
+                {"params": self.model.head.parameters(), "lr": opt_hparams.get("head_lr", 1e-4)}
+            ]
+        optimizer = torch.optim.AdamW(params, weight_decay=opt_hparams.get("weight_decay", 1e-3))
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode=scheduler_hparams.get("mode", "min"),
+            factor=scheduler_hparams.get("factor", 0.5),
+            patience=scheduler_hparams.get("patience", 5),
+            min_lr=scheduler_hparams.get("min_lr", 1e-8)
+            )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": "val/loss"
+            }
+        }
--- py/main_prithvi.py
+++ py/main_prithvi.py
@@ -1,5 +1,6 @@
 from datamodule import WUSUSegmentationDataModule
 from terratorch.tasks import SemanticSegmentationTask
+from custom_tasks import DiffLRSemanticSegmentationTask
 import lightning.pytorch as pl
 from lightning.pytorch import seed_everything
 from lightning.pytorch import loggers as pl_loggers
@@ -11,6 +12,12 @@ from pathlib import Path
 warnings.filterwarnings("ignore", category=NotGeoreferencedWarning) # ignoro i warning di rasterio sulle immagini non georeferenziate
 logging.getLogger("tensorboardX").setLevel(logging.WARNING) # imposto il livello di logging di tensorboard per non mostrare nulla al di sotto di un warning (tipo i messaggi INFO)
 
+# UPerNet: https://huggingface.co/docs/transformers/en/model_doc/upernet
+
+ENCODER_LR = 1e-5
+DECODER_LR = HEAD_LR = 1e-3 # using decoder lr for neck too
+WEIGHT_DECAY = 1e-3
+
 def main():
     seed_everything(42, workers=True) # per riproducibilità
     REPO_ROOT = Path(__file__).parent.parent
@@ -21,7 +28,7 @@ def main():
     datamodule = WUSUSegmentationDataModule(
         data_root=DATA_ROOT,
         class_mapping_path=CLASS_MAPPING_PATH,
-        batch_size=2,
+        batch_size=1,
         num_workers=4
     )
     num_classes = len(datamodule.class_names)
@@ -29,13 +36,15 @@ def main():
     # Definisco il modello con un dizionario le cui chiavi sono gli argomenti di EncoderDecoderFactory.build_model
     model_args = {
         "backbone": "prithvi_eo_v2_600",
-        "decoder": "FPN",
+        "decoder": "UperNetDecoder", 
         "num_classes": num_classes,
-        "necks": [
-            {"name": "SelectIndices", "indices": [5, 11, 17, 23]},
-            {"name": "ReshapeTokensToImage"},
-            {"name": "LearnedInterpolateToPyramidal"}
-        ],
+        #"necks": [
+        # these are to transform prithvi tokens to image-like tensors, to make prithvi backbone compatible 
+        # with convolutional decoders that expect a piramyd of feature maps
+        #    {"name": "SelectIndices", "indices": [5, 11, 17, 23]},
+        #    {"name": "ReshapeTokensToImage"},
+        #    {"name": "LearnedInterpolateToPyramidal"}
+        #],
         "backbone_kwargs": {
             "pretrained": True,
             "in_channels": 4,
@@ -43,7 +52,11 @@ def main():
             "num_frames": 1
         },
         "decoder_kwargs": {
-            "channels": [256, 128, 64, 32]
+            # dimensioni delle feature map prodotte dal neck per i convolutional decoders.
+            # devono matchare numero e dimensioni attese dalle skip connection del decoder
+            # 256 per il token in output dal 5° transformer block della backbone,
+            # 128 per il token in output dall'11° transformer block della backbone ecc...
+            #"channels": [256, 128, 64, 32]
         },
         "head_kwargs": {
             #"out_channels": num_classes
@@ -51,15 +64,24 @@ def main():
         }
     }
 
+    # parametri custom per l'ottimizzatore con lr differenziati
+    custom_opt_params = {
+        "encoder_lr": ENCODER_LR,
+        "decoder_lr": DECODER_LR,
+        "head_lr": HEAD_LR,
+        "weight_decay": WEIGHT_DECAY
+        }
+    
     # Inizializzo il task di segmentazione semantica
-    task = SemanticSegmentationTask(
+    task = DiffLRSemanticSegmentationTask(
         model_args=model_args,                      # Argomenti per la ModelFactory
         model_factory="EncoderDecoderFactory",      # ModelFactory class che "assembla" backbone, neck, decoder, head
         loss='ce',                                  # cross-entropy loss
-        lr=1e-5,                                    # learning rate
+        lr=1e-5,                                    # learning rate globale (utilizzato per i gruppi di parametri per 
+                                                    #           i quali non viene specificato un lr)
         optimizer="AdamW",                          # ottimizzatore AdamW
-        optimizer_hparams={"weight_decay": 0.001},   # dizionario di iperparametri per opt
-        scheduler="ReduceLROnPlateau",           # lr scheduler 
+        optimizer_hparams=custom_opt_params,        # dizionario di iperparametri per opt
+        scheduler="ReduceLROnPlateau",              # lr scheduler 
         scheduler_hparams={
             "mode": "min",
             "factor": 0.5,
@@ -77,7 +99,7 @@ def main():
     # Definisco un logger per dare un nome all'esperimento
     logger = pl_loggers.TensorBoardLogger(
         save_dir= REPO_ROOT / "lightning_logs",
-        name=f"{model_args['backbone']}_{model_args['decoder']}_finetune_wusu_means"
+        name=f"{model_args['backbone']}_{model_args['decoder']}_wmeans_diffLRs"
     )
 
     # Definisco il trainer
@@ -88,7 +110,7 @@ def main():
         #overfit_batches=None,           # per debug: usa solo un batch (sia train che val) per overfitting
         log_every_n_steps=10,         # logga scalar metrics ogni n batch di training (default 50, mettere 1 per test overfit batches)
         precision="16-mixed",        # usa precisione mista (float16 e float32) per risparmiare memoria GPU
-        accumulate_grad_batches=4,  # numero di batch da accumulare prima di un passo di ottimizzazione:
+        accumulate_grad_batches=8,  # numero di batch da accumulare prima di un passo di ottimizzazione:
                                     # batch_size=2 e accumulate_grad_batches=4 => effettivo batch size 8,
                                     # utile se batch_size è limitato dalla memoria GPU
         callbacks=[EarlyStopping(
